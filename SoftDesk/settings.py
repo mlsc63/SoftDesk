e7c9a813ce75f0b0d85818a53092feda55b908b9
mlescure <mlescure@mlesucre.fr>
2021-07-19 17:30:36 +0200

    MAJ 19/07

--- SoftDesk/settings.py
+++ SoftDesk/settings.py
@@ -140,10 +140,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ]
-
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',),
 }
 
 AUTH_USER_MODEL = 'user.User'
