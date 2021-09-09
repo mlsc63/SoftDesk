ba3a15cf16e6eec3eed4dddff19a75a14eae2ef6
mlescure <mlescure@mlesucre.fr>
2021-09-09 12:12:33 +0200

    add requirements.txt

--- comment/views.py
+++ comment/views.py
@@ -1,4 +1,3 @@
-
 from rest_framework import viewsets
 from .serializers import CommentSerializer
 from .models import Comments
--- contributor/serializers.py
+++ contributor/serializers.py
@@ -5,17 +5,12 @@ from rest_framework import serializers
 class ContributorSerializer(serializers.ModelSerializer):
     project = serializers.ReadOnlyField(source='project.id')
 
-
-
     def create(self, validated_data):
         print('create')
         contributor = Contributor.objects.create(**validated_data)
         contributor.save()
         return contributor
 
-
-
     class Meta:
         model = Contributor
         fields = ['id', 'user_id', 'project', 'permission', 'role']
-
